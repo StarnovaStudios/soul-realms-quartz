d70eaafde3bd095c0cac8086a96cf9477b72aaa6
JosTheDude <43651265+JosTheDude@users.noreply.github.com>
2025-08-14 12:45:24 -0400

    feat: branding

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -8,8 +8,17 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
 
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <h1>⚠ Rift Detected | 404 Page Not Found</h1>
+      {/* <p>{i18n(cfg.locale).pages.error.notFound}</p> */}
+      <p>
+        The page you seek lies beyond the Void.
+        Either the knowledge has been hidden or
+        this fragment of the Realm has yet to be woven into existence.
+      </p>
+      <p>
+        Travelers speak of realms shifting, gates sealing, and memories fading. 
+        Are you in the right place?
+      </p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
