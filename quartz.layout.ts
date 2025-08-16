6a088f8076d8c680ba6e73aa807fdfdaef523cb9
Barnav (Jos) M. <43651265+JosTheDude@users.noreply.github.com>
2025-08-16 15:15:03 -0400

    feat: explorer title rename -> "Navigator"

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,10 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
-Component.Explorer({
-  title: "Navigator",
-})
-
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
@@ -43,7 +39,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "Navigator",
+    }),
   ],
   right: [
     Component.Graph(),
@@ -67,7 +65,9 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "Navigator",
+    }),
   ],
   right: [],
 }
